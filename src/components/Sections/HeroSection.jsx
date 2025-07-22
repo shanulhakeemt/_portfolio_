2ef0882d7650dbcbc8f03ab3a1755313f703710c
shanulhakeemt <shanulhakeem60@gmail.com>
2025-07-22 23:08:28 +0530

    .

--- src/components/Sections/HeroSection.jsx
+++ src/components/Sections/HeroSection.jsx
@@ -2,7 +2,7 @@ import { motion, useScroll, useTransform } from "framer-motion";
 import { ArrowDown, Mail } from "lucide-react";
 import { FiGithub, FiLinkedin } from "react-icons/fi";
 import { useTheme } from "../../context/ThemeContext";
-import PROFILE_PIC from "../../assets/images/p.jpeg";
+import PROFILE_PIC from "../../assets/Images/p.jpeg";
 
 function HeroSection() {
   const { isDarkMode } = useTheme();
